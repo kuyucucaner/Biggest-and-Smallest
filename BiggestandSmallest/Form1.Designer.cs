cfcd2ac89fe7be6ba7971e46c19afcf1870efb6c
kuyucucaner <141584156+kuyucucaner@users.noreply.github.com>
2023-08-20 17:33:24 +0300

    Add files via upload

---
+++ BiggestandSmallest/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace BiggestandSmallest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonBul = new System.Windows.Forms.Button();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.txtSayi3 = new System.Windows.Forms.TextBox();
+            this.txtEnBuyuk = new System.Windows.Forms.TextBox();
+            this.txtEnKucuk = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(42, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1.Sayı :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(42, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2.Sayı :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(42, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "3.Sayı :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(42, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Büyük";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(42, 323);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Küçük";
+            // 
+            // buttonBul
+            // 
+            this.buttonBul.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonBul.Location = new System.Drawing.Point(134, 216);
+            this.buttonBul.Name = "buttonBul";
+            this.buttonBul.Size = new System.Drawing.Size(115, 23);
+            this.buttonBul.TabIndex = 5;
+            this.buttonBul.Text = "BUL";
+            this.buttonBul.UseVisualStyleBackColor = true;
+            this.buttonBul.Click += new System.EventHandler(this.buttonBul_Click);
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(134, 70);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(115, 20);
+            this.txtSayi1.TabIndex = 6;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(134, 122);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(115, 20);
+            this.txtSayi2.TabIndex = 7;
+            // 
+            // txtSayi3
+            // 
+            this.txtSayi3.Location = new System.Drawing.Point(134, 179);
+            this.txtSayi3.Name = "txtSayi3";
+            this.txtSayi3.Size = new System.Drawing.Size(115, 20);
+            this.txtSayi3.TabIndex = 8;
+            // 
+            // txtEnBuyuk
+            // 
+            this.txtEnBuyuk.Enabled = false;
+            this.txtEnBuyuk.Location = new System.Drawing.Point(134, 274);
+            this.txtEnBuyuk.Name = "txtEnBuyuk";
+            this.txtEnBuyuk.Size = new System.Drawing.Size(115, 20);
+            this.txtEnBuyuk.TabIndex = 9;
+            // 
+            // txtEnKucuk
+            // 
+            this.txtEnKucuk.Enabled = false;
+            this.txtEnKucuk.Location = new System.Drawing.Point(134, 323);
+            this.txtEnKucuk.Name = "txtEnKucuk";
+            this.txtEnKucuk.Size = new System.Drawing.Size(115, 20);
+            this.txtEnKucuk.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.ClientSize = new System.Drawing.Size(403, 416);
+            this.Controls.Add(this.txtEnKucuk);
+            this.Controls.Add(this.txtEnBuyuk);
+            this.Controls.Add(this.txtSayi3);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.buttonBul);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonBul;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.TextBox txtSayi3;
+        private System.Windows.Forms.TextBox txtEnBuyuk;
+        private System.Windows.Forms.TextBox txtEnKucuk;
+    }
+}
+
